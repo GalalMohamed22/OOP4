edaf11d7a7708ff25fa0bb2a1f1d1167d4b88e4f
GalalMohamed22 <galal9678@gmail.com>
2026-09-11 03:43:25 +0300

    Question01

--- AssignmentOOP4/Program.cs
+++ AssignmentOOP4/Program.cs
@@ -6,7 +6,12 @@ namespace AssignmentOOP4
     {
         static void Main(string[] args)
         {
-            
+            #region  Q1
+
+            // A) Abstraction is the process of hiding unnecessary implementation details and showing only the essential features of an object
+            // B) because it reduces complexity by hiding implementation details and exposing only the essential functionality, making code easier to understand, maintain, and extend
+
+            #endregion
 
         }
     }
